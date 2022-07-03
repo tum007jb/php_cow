5ed4ffa89d2707a82bc4c79c67dbc82b63a08e1c
Surayos <surayos@mfec.co.th>
2022-07-03 09:45:37 +0700

    fix

--- mysql_cow/run_mysql_docker.sh
+++ mysql_cow/run_mysql_docker.sh
@@ -3,6 +3,6 @@ docker run \
 --name=mysql-cow \
 --env="MYSQL_ROOT_PASSWORD=P@ssw0rd@cow" \
 --publish 3306:3306 \
---volume=/root/php_cow/mysql_cow/mysql_cow_config.conf/conf.d:/etc/mysql/conf.d \
+--volume=/root/php_cow/mysql_cow/mysql_cow_config.conf:/etc/mysql/conf.d \
 --volume=/var/database_mysql_storage:/var/lib/mysql \
 mysql
